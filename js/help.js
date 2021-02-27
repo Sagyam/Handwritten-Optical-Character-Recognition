7c37d09951816ab9b5994cb5baccfd4b4ba490f6
Sagyam Thapa <sagyamthapa32@gmail.com>
2021-02-27 16:02:45 +0545

    CSS styling

---
+++ js/help.js
@@ -0,0 +1,26 @@
+function generateTableHead(table, data) {
+	let thead = table.createTHead();
+	let row = thead.insertRow();
+	for (let key of data) {
+		let th = document.createElement("th");
+		let text = document.createTextNode(key);
+		th.appendChild(text);
+		row.appendChild(th);
+	}
+}
+
+function generateTable(table, data) {
+	for (let element of data) {
+		let row = table.insertRow();
+		for (key in element) {
+			let cell = row.insertCell();
+			let text = document.createTextNode(element[key]);
+			cell.appendChild(text);
+		}
+	}
+}
+
+let table = document.getElementById("table");
+let data = ["Character ", " Symbol"];
+generateTableHead(table, data);
+generateTable(table, map);
