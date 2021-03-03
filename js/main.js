b618c6b475b0f4d134edeccc189e3a333a98a666
Sagyam Thapa <sagyamthapa32@gmail.com>
2021-03-03 09:14:49 +0545

    added save button

--- js/main.js
+++ js/main.js
@@ -206,3 +206,22 @@ function showStack() {
 
 	stack.innerHTML = text;
 }
+
+function save() {
+	let name = makeName(8);
+
+	var png = ReImg.fromCanvas(document.getElementById("canvas")).downloadPng(
+		name
+	);
+}
+
+function makeName(length) {
+	var result = "";
+	var characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
+}
