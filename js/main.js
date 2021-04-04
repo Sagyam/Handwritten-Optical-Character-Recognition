36f49cedc07f0a39939cf68ff75ec4b21c027217
Sagyam Thapa <sagyamthapa32@gmail.com>
2021-04-04 20:33:52 +0545

    added touch support

--- js/main.js
+++ js/main.js
@@ -85,6 +85,50 @@ window.onload = () => {
 		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 		removePrediction();
 	}
+
+	// Set up touch events for mobile, etc
+	canvas.addEventListener(
+		"touchstart",
+		function (e) {
+			mousePos = getTouchPos(canvas, e);
+			var touch = e.touches[0];
+			var mouseEvent = new MouseEvent("mousedown", {
+				clientX: touch.clientX,
+				clientY: touch.clientY,
+			});
+			canvas.dispatchEvent(mouseEvent);
+		},
+		false
+	);
+	canvas.addEventListener(
+		"touchend",
+		function (e) {
+			var mouseEvent = new MouseEvent("mouseup", {});
+			canvas.dispatchEvent(mouseEvent);
+		},
+		false
+	);
+	canvas.addEventListener(
+		"touchmove",
+		function (e) {
+			var touch = e.touches[0];
+			var mouseEvent = new MouseEvent("mousemove", {
+				clientX: touch.clientX,
+				clientY: touch.clientY,
+			});
+			canvas.dispatchEvent(mouseEvent);
+		},
+		false
+	);
+
+	// Get the position of a touch relative to the canvas
+	function getTouchPos(canvasDom, touchEvent) {
+		var rect = canvasDom.getBoundingClientRect();
+		return {
+			x: touchEvent.touches[0].clientX - rect.left,
+			y: touchEvent.touches[0].clientY - rect.top,
+		};
+	}
 };
 
 function predict() {
